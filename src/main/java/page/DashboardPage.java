009cf00ad02a29951e3f131f76aabf5265e434fa
mandiosho <60794614+mandiosho@users.noreply.github.com>
2020-05-19 11:36:13 -0500

    newbranch1

--- src/main/java/page/DashboardPage.java
+++ src/main/java/page/DashboardPage.java
@@ -1,5 +1,42 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class DashboardPage {
+	WebDriver driver;
+	//create constructor 
+	//passing local driver
+	public DashboardPage(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+	
+	
+	//element library
+	@FindBy(how=How.XPATH, using = "//span[contains(text(),'Dashboard')]")
+	WebElement DASHBOARD_TAB_LOCATOR;
+	
+	
+	//method
+	
+
+
+	public void dashboardPageVisibility() {
+		wetForElement(driver,2,DASHBOARD_TAB_LOCATOR);
+		DASHBOARD_TAB_LOCATOR.click();
+		
+	}
+
+
+	private void wetForElement(WebDriver driver, int time, WebElement locator) {
+WebDriverWait wait =new WebDriverWait(driver,time);
+wait.until(ExpectedConditions.visibilityOf(DASHBOARD_TAB_LOCATOR));
+		
+	}
 
 }
--- src/test/java/test/DashboardTest.java
+++ src/test/java/test/DashboardTest.java
@@ -1,5 +1,29 @@
 package test;
 
-public class DashboardTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
-}
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+
+public class DashboardTest {
+	WebDriver driver;
+	@Test
+	public void dashboardVerification() {
+		// connect with BrowserFactory
+				driver = BrowserFactory.init();
+
+				// create page factory to call the Page
+				LoginPage LoginPage = PageFactory.initElements(driver, LoginPage.class);
+				DashboardPage DashboardPage = PageFactory.initElements(driver, DashboardPage.class);
+				// call the methods
+				LoginPage.enterUserName("techfiosdemo@gmail.com");
+				LoginPage.enterPassword("abc123");
+				LoginPage.clickOnSignInButton();
+				DashboardPage.dashboardPageVisibility();
+
+
+
+}}
